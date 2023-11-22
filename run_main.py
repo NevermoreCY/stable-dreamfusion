14b41eb313179328591d3c5dab9d09abbf83ce07
nev <cy823236858@gmail.com>
2023-11-22 13:12:16 +0800

    developing

--- run_main.py
+++ run_main.py
@@ -14,6 +14,8 @@ def doArgs(argList):
     # parser.add_argument('--output', action="store", dest="outputFn", type=str, help="Output file name", required=True)
     parser.add_argument('--color', type=str, help="whether to add color string", default=False)
 
+    parser.add_argument('--total_num', type=int, help="whether to add color string", required=True)
+
     return parser.parse_args(argList)
 
 
@@ -27,11 +29,18 @@ def main():
     folders = '/yuch_ws/zero123/objaverse-rendering/views_shape'
     good_path = '/yuch_ws/zero123/zero123/good_samples.json'
 
+    total_num = args.total_num
+
     with open(good_path,'r') as f:
         folder_list = json.load(f)
 
+    # with open('histo')
     for folder in folder_list:
-        histo_count[folder] = 0
+        histo_count[folder] = total_num
+    #
+    # with open('hist_count.json','r') as f :
+    #     histo_count = json.load(f)
+    #
 
     start_idx = job_num* 10
     end_idx = (job_num+1)* 10
@@ -51,6 +60,7 @@ def main():
             color_id = random.randint(0,len(color_list)-1)
 
             work_space = 'results/control3d_' + color_list[color_id] + '_' + prompt + '_' + folder + '_' + str(histo_count[folder])
+            # histo_count[folder] += 1
 
             prompt = color_list[color_id] + ' ' + prompt
             iters = 10000
@@ -83,8 +93,8 @@ def main():
             test_cmd = 'python3 main.py -O --workspace ' + work_space + ' --test --save_mesh --zero123_ckpt pretrained/zero123/control_3d.ckpt --save_guidance --save_guidance_interval 10'
             print('****** Test cmd is ', test_cmd)
             os.system(test_cmd)
-    with open('hist_count.json',w) as f:
-        json.dump()
+    # with open('hist_count.json','w') as f:
+    #     json.dump(histo_count,f)
 
 if __name__ == '__main__':
     #sys.argv = ["programName.py","--input","test.txt","--output","tmp/test.txt"]

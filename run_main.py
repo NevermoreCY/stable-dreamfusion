a23aa71d5e17b925d84854496692dc22ff230088
nev <cy823236858@gmail.com>
2023-11-22 12:22:49 +0800

    developing

--- run_main.py
+++ run_main.py
@@ -49,25 +49,42 @@ def main():
 
         if color:
             color_id = random.randint(len(color_list)-1)
+
+            work_space = 'results/control3d_' + color_list[color_id] + '_' + prompt + '_' + folder + '_' + str(histo_count[folder])
+
             prompt = color_list[color_id] + ' ' + prompt
-        work_space = 'results/control3d_' + prompt + '_'+ folder + '_'+ str(histo_count[folder])
-        iters = 10000
+            iters = 10000
+            # train
+            train_cmd = ('python3 main.py -O --image ' + img_path + ' --workspace ' + work_space + ' --iters ' + str(
+                iters) +
+                         ' --control_text ' +'\"'+ prompt + '\"' + ' --zero123_ckpt pretrained/zero123/control_3d.ckpt --save_guidance --save_guidance_interval 10')
 
+            print('****** Train cmd is ', train_cmd)
+            os.system(train_cmd)
+            print("****** training done, start testing")
+            # test
 
+            test_cmd = 'python3 main.py -O --workspace ' + work_space + ' --test --save_mesh --zero123_ckpt pretrained/zero123/control_3d.ckpt --save_guidance --save_guidance_interval 10'
+            print('****** Test cmd is ', test_cmd)
+            os.system(test_cmd)
+        else:
 
-        # train
+            work_space = 'results/control3d_' + prompt + '_'+ folder + '_'+ str(histo_count[folder])
+            iters = 10000
+            # train
+            train_cmd = ('python3 main.py -O --image '+ img_path + ' --workspace ' + work_space+ ' --iters ' + str(iters) +
+                   ' --control_text ' + prompt + ' --zero123_ckpt pretrained/zero123/control_3d.ckpt --save_guidance --save_guidance_interval 10')
 
-        train_cmd = ('python3 main.py -O --image '+ img_path + ' --workspace ' + work_space+ ' --iters ' + str(iters) +
-               ' --control_text ' + prompt + ' --zero123_ckpt pretrained/zero123/control_3d.ckpt --save_guidance --save_guidance_interval 10')
+            print('****** Train cmd is ', train_cmd)
+            os.system(train_cmd)
+            print("****** training done, start testing")
+            # test
 
-        print('****** Train cmd is ', train_cmd)
-        os.system(train_cmd)
-        print("****** training done, start testing")
-        # test
-
-        test_cmd = 'python3 main.py -O --workspace ' + work_space + ' --test --save_mesh --zero123_ckpt pretrained/zero123/control_3d.ckpt --save_guidance --save_guidance_interval 10'
-        print('****** Test cmd is ', test_cmd)
-        os.system(test_cmd)
+            test_cmd = 'python3 main.py -O --workspace ' + work_space + ' --test --save_mesh --zero123_ckpt pretrained/zero123/control_3d.ckpt --save_guidance --save_guidance_interval 10'
+            print('****** Test cmd is ', test_cmd)
+            os.system(test_cmd)
+    with open('hist_count.json',w) as f:
+        json.dump()
 
 if __name__ == '__main__':
     #sys.argv = ["programName.py","--input","test.txt","--output","tmp/test.txt"]
